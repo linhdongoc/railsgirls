de95dfaecfa56615203b91a5d41153e7c06bd2cd
linhdongoc <linhhoa.do@gmail.com>
2018-09-29 21:49:00 +0200

    add unit test for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
 
   belongs_to :idea
+
+  validates :user_name, presence: true
+  validates :body, presence: true
+  validates :idea_id, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 describe Comment do
-  it 'has an username and a body and belongs to an idea' do
-    idea = Idea.create!(name: 'My Awesome Idea Name', description: 'My Awesome Idea Name. Yeah!')
-    comment = idea.comments.create(user_name: 'user1', body: 'This is my first comment')
+  context 'validations' do
+    before(:all) do
+      @comment1 = create(:comment)
+    end
 
-    expect(comment.user_name).to eq('user1')
-    expect(comment.body).to eq('This is my first comment')
+    it 'is valid with valid attributes' do
+      expect(@comment1).to be_valid
+    end
+
+    it 'is invalid without user_name' do
+      comment2 = build(:comment, user_name: nil)
+      expect(comment2).to_not be_valid
+    end
+
+    it 'is invalid without body content' do
+      comment2 = build(:comment, body: nil)
+      expect(comment2).to_not be_valid
+    end
+
+    it 'is invalid without idea' do
+      comment2 = build(:comment2, idea_id: nil)
+      expect(comment2).to_not be_valid
+    end
   end
 end
