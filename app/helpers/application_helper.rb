b9e1ac61767d6863315098a8c8abc492a1fe9396
linhdongoc <linhhoa.do@gmail.com>
2018-10-06 15:10:30 +0200

    add breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def breadcrumbs_tag(chunks = [])
+    if chunks
+      chunks.unshift(link_to('home', root_path))
+
+      current    = content_tag(:li, chunks.pop, class: 'breadcrumb-item current')
+      previous   = content_tag(:li, chunks.pop, class: 'breadcrumb-item previous')
+      chunk_tags = chunks.map{ |c| content_tag(:li, c, class: 'breadcrumb-item') }.push(previous, current).join.html_safe
+
+      content_tag(:ul, chunk_tags, class: 'breadcrumbs')
+    end
+  end
 end
