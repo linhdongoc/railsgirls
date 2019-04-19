dfdaff5334d2422464d1390ccf343598c5ecc4b5
linhdongoc <linhhoa.do@gmail.com>
2019-04-19 16:42:07 +0200

    re-style views

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -2,7 +2,6 @@ class RoomMessagesController < ApplicationController
   before_action :set_room
 
   def create
-    debugger
     @room_message = RoomMessage.create(user: current_user, room: @room, message: params.dig(:room_message, :message))
     RoomChannel.broadcast_to(@room, @room_message)
   end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -15,6 +15,7 @@ class RoomsController < ApplicationController
   def show
     @room_message = RoomMessage.new(room: @room)
     @room_messages = @room.room_messages.includes(:user)
+    @rooms = Room.all
   end
 
   def create
